340b888fa3bb591fd4b504abdfa0d14779fbcaa5
Christian Kruse <cjk@defunct.ch>
2013-11-06 00:48:36 +0100

    added support for byte compiled init files

--- init.el
+++ init.el
@@ -3,14 +3,14 @@
 (setq load-path  (cons (expand-file-name "~/.emacs.d/site-lisp/") load-path))
 (load "~/.emacs.d/package.el")
 
-(load "~/.emacs.d/emulate-mac-keyboard-mode.el")
+(load "~/.emacs.d/emulate-mac-keyboard-mode")
 (emulate-mac-german-keyboard-mode)
 
 (dolist (f (directory-files "~/.emacs.d/init/" t "\\.el$"))
-  (load f))
+  (load (replace-regexp-in-string "\\.el$" "" f)))
 
-(load "~/.emacs.d/tweeks.el")
-(load "~/.emacs.d/keybinding.el")
+(load "~/.emacs.d/tweeks")
+(load "~/.emacs.d/keybinding")
 
 (server-start)
 
