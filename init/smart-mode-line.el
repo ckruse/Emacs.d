d81ecfe95850aeae7098ea4d6c8d16404b457d5d
Christian Kruse <cjk@defunct.ch>
2013-11-08 23:22:42 +0100

    hide more minor modes

--- init/smart-mode-line.el
+++ init/smart-mode-line.el
@@ -5,7 +5,7 @@
 (require 'smart-mode-line)
 
 (custom-set-variables
- '(sml/hidden-modes (quote (" hl-p" " my-keys" " pair" " HRB" " AC" " GitGutter" " Flymake")))
+ '(sml/hidden-modes (quote (" hl-p" " my-keys" " pair" " HRB" " AC" " GitGutter" " Flymake" " yas" " SP")))
  '(sml/override-theme nil)
  )
 
