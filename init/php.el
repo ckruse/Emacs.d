d29cce6943062b5202f1725c43b82594722aa2ab
Christian Kruse <cjk@defunct.ch>
2013-12-05 09:48:58 +0100

    wtf, why four spaces?

--- init/php.el
+++ init/php.el
@@ -1,8 +1,8 @@
 (defun ck-init-php ()
-  (setq c-basic-offset 4)
+  (setq c-basic-offset 2)
   (c-set-offset 'arglist-cont 0)
-  (c-set-offset 'arglist-intro 4)
-  (c-set-offset 'case-label 4)
+  (c-set-offset 'arglist-intro 2)
+  (c-set-offset 'case-label 2)
   (c-set-offset 'arglist-close 0)
 )
 (autoload 'php-mode
