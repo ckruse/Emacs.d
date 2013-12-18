80380ab524ca09bae3ab6cec520d2df147a21848
Christian Kruse <cjk@defunct.ch>
2013-12-18 10:08:24 +0100

    now using projectile-project-root for ffip

--- init/ffip.el
+++ init/ffip.el
@@ -6,6 +6,9 @@
 (setq projectile-indexing-method 'find
       projectile-enable-caching t)
 
+(setq ffip-project-root-function 'projectile-project-root)
+(setq-default ffip-project-root-function 'projectile-project-root)
+
 (global-set-key "\C-p" 'projectile-find-file)
 
 ;; eof
