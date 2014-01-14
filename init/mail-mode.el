4f3bc354e7d7b231077aea13c2fd8ffdf3e0c3a7
Christian Kruse <cjk@defunct.ch>
2014-01-14 10:58:27 +0100

    different coloring for different quoting levels

--- init/mail-mode.el
+++ init/mail-mode.el
@@ -3,4 +3,22 @@
 (add-to-list 'auto-mode-alist '("mutt-" . mail-mode))
 (add-hook 'mail-mode-hook 'flyspell-mode)
 
+
+(defface mail-double-quoted-text-face
+  '((((class color)) :foreground "SteelBlue")) "Double-quoted email.")
+(defface mail-treble-quoted-text-face
+  '((((class color)) :foreground "SlateGrey")) "Treble-quoted email.")
+(defface mail-multiply-quoted-text-face
+  '((((class color)) :foreground "DarkSlateGrey")) "Multiply-quoted email.")
+
+(font-lock-add-keywords 'mail-mode
+                        '(("^\\(\\( *>\\)\\{4,\\}\\)\\(.*\\)$"
+                           (1 'font-lock-comment-delimiter-face)
+                           (3 'mail-multiply-quoted-text-face))
+                          ("^\\(\\( *>\\)\\{3\\}\\)\\(.*\\)$"
+                           (1 'font-lock-comment-delimiter-face)
+                           (3 'mail-treble-quoted-text-face))
+                          ("^\\( *> *>\\)\\(.*\\)$"
+                           (1 'font-lock-comment-delimiter-face)
+                           (2 'mail-double-quoted-text-face))))
 ;; eof
