0b8e7db0c4d9f00943b42e14df47d0857c7d0f5c
Christian Kruse <cjk@defunct.ch>
2014-02-25 13:14:11 +0100

    no longer using dropbox/btsync/whatever to sync to mobile-org

--- init/org-mode.el
+++ init/org-mode.el
@@ -3,10 +3,10 @@
 
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\)$" . org-mode))
 
-(setq org-directory "~/BTSync/org")
-(setq org-mobile-directory "~/BTSync/MobileOrg/")
-(setq org-agenda-files (quote ("~/BTSync/org/inbox.org" "~/BTSync/org/projects.org")))
-(setq org-mobile-inbox-for-pull "~/BTSync/org/inbox.org")
+(setq org-directory "~/Dokumente/org")
+(setq org-mobile-directory "/scpc:ckruse@jugulator.defunced.de:/home/ckruse/org")
+(setq org-agenda-files (quote ("~/Dokumente/org/inbox.org" "~/Dokumente/org/projects.org")))
+(setq org-mobile-inbox-for-pull "~/Dokumente/org/inbox.org")
 (setq org-default-notes-file (concat org-directory "/inbox.org"))
 
 (add-hook 'org-mode-hook 'turn-on-font-lock)
