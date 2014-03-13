e9a9c771d8d8753ca1dddd49ea5c7819b64e4956
Christian Kruse <cjk@defunct.ch>
2014-03-13 10:36:00 +0100

    fixed dictionary name

--- init/flyspell.el
+++ init/flyspell.el
@@ -3,7 +3,7 @@
 (defun fd-switch-dictionary()
   (interactive)
   (let* ((dic ispell-current-dictionary)
-         (change (if (string= dic "de") "en" "de")))
+         (change (if (string= dic "deutsch") "en" "deutsch")))
     (ispell-change-dictionary change)
     (message "Dictionary switched from %s to %s" dic change)
     ))
