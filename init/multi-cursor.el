91facb0382319fb63c1ac59ed039c5b424ae1f33
Christian Kruse <cjk@defunct.ch>
2014-03-14 10:41:51 +0100

    moved multi-cursor keybindings to different keybindings

--- init/multi-cursor.el
+++ init/multi-cursor.el
@@ -2,9 +2,9 @@
 
 (require 'multiple-cursors)
 
-(global-set-key (kbd "C-v") 'mc/edit-lines)
-(global-set-key (kbd "C-d") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-M-d") 'mc/mark-previous-like-this)
-(global-set-key (kbd "M-d") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c v") 'mc/edit-lines)
+(global-set-key (kbd "C-c C-d") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c M-d") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c D") 'mc/mark-all-like-this)
 
 ;; eof
