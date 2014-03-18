bf579c2060552d86737b0b085ae5a4d12d389930
Christian Kruse <cjk@defunct.ch>
2014-03-18 08:36:43 +0100

    ensure that key is always set

--- init/multi-cursor.el
+++ init/multi-cursor.el
@@ -7,4 +7,21 @@
 (global-set-key (kbd "C-c M-d") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c D") 'mc/mark-all-like-this)
 
+(defun overwrite-keys-mc ()
+  (local-set-key (kbd "C-c v") 'mc/edit-lines)
+  (local-set-key (kbd "C-c C-d") 'mc/mark-next-like-this)
+  (local-set-key (kbd "C-c M-d") 'mc/mark-previous-like-this)
+  (local-set-key (kbd "C-c D") 'mc/mark-all-like-this)
+
+  (global-set-key (kbd "C-c v") 'mc/edit-lines)
+  (global-set-key (kbd "C-c C-d") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-c M-d") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c D") 'mc/mark-all-like-this))
+
+(add-hook 'web-mode-hook 'overwrite-keys-mc)
+(add-hook 'php-mode-hook 'overwrite-keys-mc)
+(add-hook 'c-mode-hook 'overwrite-keys-mc)
+(add-hook 'ruby-mode-hook 'overwrite-keys-mc)
+(add-hook 'sass-mode-hook 'overwrite-keys-mc)
+
 ;; eof
