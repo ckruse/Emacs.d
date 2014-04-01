51d066edfb3be5eab3c997a25815cee894c2b1e1
Christian Kruse <cjk@defunct.ch>
2014-04-01 17:05:54 +0200

    updated initialization code

--- init/smart-mode-line.el
+++ init/smart-mode-line.el
@@ -1,12 +1,10 @@
 ;; -*- coding: utf-8 -*-
 
-(add-to-list 'load-path "~/.emacs.d/packages/smart-mode-line/")
-
 (require 'smart-mode-line)
 
 (custom-set-variables
  '(sml/hidden-modes (quote (" hl-p" " my-keys" " pair" " HRB" " AC" " GitGutter" " Flymake" " yas" " SP")))
- '(sml/override-theme nil)
+ ;'(sml/override-theme nil)
  )
 
 (add-to-list 'sml/replacer-regexp-list '("^~/[Ss]ites/" ":WEB:"))
