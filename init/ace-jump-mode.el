d3ec788d1f148af3692d511aee00287bdae4fc7d
Christian Kruse <cjk@defunct.ch>
2014-04-01 17:18:29 +0200

    removed legacy code

--- init/ace-jump-mode.el
+++ init/ace-jump-mode.el
@@ -1,10 +1,7 @@
 ;; -*- coding: utf-8 -*-
 
-(add-to-list 'load-path "~/.emacs.d/packages/ace-jump-mode/")
 (require 'ace-jump-mode)
-
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
-
 
 
 ;;
