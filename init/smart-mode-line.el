8489810338ce4a76e202fa97e38f1cbc0d1f8a4c
Christian Kruse <cjk@defunct.ch>
2014-04-02 09:17:31 +0200

    more customization for smart-mode-line

--- init/smart-mode-line.el
+++ init/smart-mode-line.el
@@ -3,12 +3,14 @@
 (require 'smart-mode-line)
 
 (custom-set-variables
- '(sml/hidden-modes (quote (" hl-p" " my-keys" " pair" " HRB" " AC" " GitGutter" " Flymake" " yas" " SP")))
+ '(sml/hidden-modes (quote (" hl-p" " my-keys" " pair" " HRB" " AC" " GitGutter" " Flymake" " yas" " SP" " WS" " MRev")))
  ;'(sml/override-theme nil)
  )
 
-(add-to-list 'sml/replacer-regexp-list '("^~/[Ss]ites/" ":WEB:"))
-(add-to-list 'sml/replacer-regexp-list '("^~/dev/" ":DEV:"))
+(add-to-list 'sml/replacer-regexp-list '("^~/\\(data/\\)?[Ss]ites/" ":WEB:"))
+(add-to-list 'sml/replacer-regexp-list '("^~/\\(data/\\)?dev/" ":DEV:"))
+(add-to-list 'sml/replacer-regexp-list '("^~/\\(data/\\)?dev/postgres/" ":PG:"))
+(add-to-list 'sml/replacer-regexp-list '("^~/\\(data/\\)?Dokumente/" ":DOC:"))
 
 (if after-init-time (sml/setup)
   (add-hook 'after-init-hook 'sml/setup))
