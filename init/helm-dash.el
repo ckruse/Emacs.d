0a867c78922b86a64fd22045bb46c4616dc4ded1
Christian Kruse <cjk@defunct.ch>
2014-04-30 09:00:56 +0200

    now helm-dash is using all the docs always

--- init/helm-dash.el
+++ init/helm-dash.el
@@ -2,54 +2,10 @@
 
 (require 'helm-dash)
 
-(defun ruby-doc ()
-  (interactive)
-  (setq-local helm-dash-docsets '("Ruby" "Ruby On Rails")))
-
-(add-hook 'ruby-mode-hook 'ruby-doc)
-
-(defun c-doc ()
-  (interactive)
-  (setq-local helm-dash-docsets '("C" "C++")))
-
-(add-hook 'c-mode-hook 'c-doc)
-(add-hook 'c++-mode-hook 'c-doc)
-
-(defun web-mode-doc ()
-  (interactive)
-  (setq-local helm-dash-docsets '("HTML" "JavaScript" "CSS" "Ruby" "PHP" "Ruby on Rails")))
-
-(add-hook 'web-mode-hook 'web-mode-doc)
-
-(defun js-mode-doc ()
-  (interactive)
-  (setq-local helm-dash-docsets '("JavaScript")))
-
-(add-hook 'js-mode-hook 'js-mode-doc)
-
-(defun php-mode-doc ()
-  (interactive)
-  (setq-local helm-dash-docsets '("PHP")))
-
-(add-hook 'php-mode-hook 'php-mode-doc)
-
-(defun sql-mode-doc ()
-  (interactive)
-  (setq-local helm-dash-docsets '("PostgreSQL")))
-
-(add-hook 'sql-mode-hook 'sql-mode-doc)
-
-(defun sh-mode-doc ()
-  (interactive)
-  (setq-local helm-dash-docsets '("Bash")))
-
-(add-hook 'sh-mode-hook 'sh-mode-doc)
-
-(defun emacs-lisp-mode-doc ()
-  (interactive)
-  (setq-local helm-dash-docsets '("Emacs Lisp")))
-
-(add-hook 'emacs-lisp-mode-hook 'emacs-lisp-mode-doc)
+(setq helm-dash-docsets '("Bash" "Emacs_Lisp" "PostgreSQL"
+                          "HTML" "JavaScript" "CSS" "C"
+                          "C++" "PHP" "Ruby" "Ruby_on_Rails_3"
+                          "Ruby_on_Rails_4"))
 
 (global-set-key [f1] 'helm-dash-at-point)
 
