67ca32792f61dea220f3877fd842352eedc04878
Christian Kruse <cjk@defunct.ch>
2014-05-26 11:33:54 +0200

    added jquery docs to helm-dash

--- init/helm-dash.el
+++ init/helm-dash.el
@@ -5,7 +5,7 @@
 (setq helm-dash-docsets '("Bash" "Emacs_Lisp" "PostgreSQL"
                           "HTML" "JavaScript" "CSS" "C"
                           "C++" "PHP" "Ruby" "Ruby_on_Rails_3"
-                          "Ruby_on_Rails_4"))
+                          "Ruby_on_Rails_4" "jQuery"))
 
 (global-set-key [f1] 'helm-dash-at-point)
 
