523362471322558dfae6eb065a5c6d7868e51a96
Christian Kruse <cjk@defunct.ch>
2014-08-05 17:48:26 +0200

    fixing ruby indention of lines continueing a comma

--- init/ruby.el
+++ init/ruby.el
@@ -16,4 +16,27 @@
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.prawn$" . ruby-mode))
 
+
+(defadvice ruby-indent-line (after line-up-args activate)
+  (let (indent prev-indent arg-indent)
+    (save-excursion
+      (back-to-indentation)
+      (when (zerop (car (syntax-ppss)))
+        (setq indent (current-column))
+        (skip-chars-backward " \t\n")
+        (when (eq ?, (char-before))
+          (ruby-backward-sexp)
+          (back-to-indentation)
+          (setq prev-indent (current-column))
+          (skip-syntax-forward "w_.")
+          (skip-chars-forward " ")
+          (setq arg-indent (+ (ruby-current-indentation) ruby-indent-level))))) ;; (current-column)
+    (when prev-indent
+      (let ((offset (- (current-column) indent)))
+        (cond ((< indent prev-indent)
+               (indent-line-to prev-indent))
+              ((= indent prev-indent)
+               (indent-line-to arg-indent)))
+        (when (> offset 0) (forward-char offset))))))
+
 ;; eof
