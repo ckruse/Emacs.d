abb80e0cf922264f95bcef50e4d63796ee663aa6
Christian Kruse <cjk@defunct.ch>
2014-09-02 23:34:23 +0200

    moving wl configuration to ~/.emacs.d/wl

--- init/wl.el
+++ init/wl.el
@@ -4,4 +4,7 @@
 (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
 (autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
 
+(custom-set-variables
+ '(wl-init-file "~/.emacs.d/wl"))
+
 ;; eof
