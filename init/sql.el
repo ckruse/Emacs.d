f535b8c3e2f3e169d707359f5ba4dac420b4f719
Christian Kruse <cjk@defunct.ch>
2014-09-23 09:47:19 +0200

    new defun to indent sql queries in strings

--- init/sql.el
+++ init/sql.el
@@ -9,4 +9,22 @@
 
 (global-set-key (kbd "C-c u") 'sqlup-capitalize-keywords-in-region)
 
+(defun sql-indent-string ()
+  "Indents the string under the cursor as SQL."
+  (interactive)
+  (save-excursion
+    (er/mark-inside-quotes)
+    (let* ((text (buffer-substring-no-properties (region-beginning) (region-end)))
+           (pos (region-beginning))
+           (column (progn (goto-char pos) (current-column)))
+           (formatted-text (with-temp-buffer
+                             (insert text)
+                             (delete-trailing-whitespace)
+                             (sql-indent-buffer)
+                             (replace-string "\n" (concat "\n" (make-string column (string-to-char " "))) nil (point-min) (point-max))
+                             (buffer-string))))
+      (delete-region (region-beginning) (region-end))
+      (goto-char pos)
+      (insert formatted-text))))
+
 ;; eof
