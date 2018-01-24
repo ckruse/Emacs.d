ea7796cbb630e64f23554a8363eb01c6847bb852
Christian Kruse <cjk@defunct.ch>
2018-01-24 11:38:36 +0100

    fix: correct URLs for archives

--- init.el
+++ init.el
@@ -2,8 +2,8 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa"        . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("org"          . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa"        . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org"          . "https://orgmode.org/elpa/") t)
 
 (package-initialize)
 
