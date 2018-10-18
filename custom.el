a7491bbb9bf5d176e10dd14b1b82ca4d22683935
Christian Kruse <christian@kruse.cool>
2018-10-18 20:06:00 +0200

    ref: cleanup

--- custom.el
+++ custom.el
@@ -6,13 +6,10 @@
  '(custom-safe-themes
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(js-indent-level 2)
  '(package-selected-packages
    (quote
-    (helm-rg lsp-typescript lsp-javascript-typescript magit paradox magit-todos editorconfig smart-mode-line lsp-css systemd flycheck-dialyxir flycheck-mix auto-package-update spaceline-all-the-icons all-the-icons highlight-indent-guides po-mode flycheck-credo alchemist elixir-mode yaml-mode js2-mode rspec-mode robe rubocop php-mode prettier-js gist projectile-rails company-ansible company-web company org-bullets org-plus-contrib diminish multiple-cursors markdown-mode flycheck git-timemachine expand-region web-mode yasnippet ace-window hydra helm-projectile projectile mwim general which-key smartparens helm spaceline use-package)))
- '(paradox-github-token t)
- '(web-mode-disable-auto-pairing t)
- '(web-mode-enable-auto-pairing nil))
+    (org-bullets org-plus-contrib general po-mode flycheck-credo alchemist elixir-mode yaml-mode rjsx-mode tide prettier-js js2-mode rspec-mode rubocop robe php-mode markdown-mode editorconfig gist projectile-rails git-timemachine magit-todos magit company-ansible company-web company flycheck multiple-cursors expand-region web-mode yasnippet ace-window helm-rg helm-projectile projectile mwim helm smartparens which-key paradox diminish smart-mode-line spacemacs-theme use-package)))
+ '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
